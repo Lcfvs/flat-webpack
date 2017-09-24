c04326d03d3eb1f829e59edc82f71f9dc05d1fc5
Lcfvs <michael.rouges@gmail.com>
2017-09-24 23:49:25 +0200

    Config fix

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,7 +1,3 @@
 'use strict';
 
-module.exports = require('flat-webpack-config').parse(__dirname);
-/*
-console.log(module.exports[0])
-process.exit();
-*/
+module.exports = require('flat-webpack').parse(__dirname);
